1be524f004b469e61452d00bfe5b961f9b5a451e
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2016-08-23 15:15:04 +0200

    fix

--- movim-app/start_container.sh
+++ movim-app/start_container.sh
@@ -5,4 +5,4 @@ docker rm $INSTANCE
 ## when db running on some external server use:
 docker run -dit -p 80:80 --expose 80 --name $INSTANCE $IMAGE /bin/bash /home/start_server.sh
 ## If you use database in docker container, uncomment and run following command instead of previous one:
-## docker run -dit -p 80:80 --expose 80 --name --link $DB_CONTAINER:$DB_HOST $INSTANCE  $IMAGE /bin/bash /home/start_server.sh
+## docker run -dit -p 80:80 --expose 80 --link $DB_CONTAINER:$DB_HOST --name $INSTANCE  $IMAGE /bin/bash /home/start_server.sh
